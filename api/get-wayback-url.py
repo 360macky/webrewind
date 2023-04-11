d0029466d2344b0d54d88126dcb8d8fabfbf63bd
Marcelo Arias <hello@marceloarias.com>
2023-04-11 15:55:40 -0500

    Wrap get-wayback-url endpoint

--- api/get-wayback-url.py
+++ api/get-wayback-url.py
@@ -1,7 +1,8 @@
 import json
+import os
 import requests
 from http.server import BaseHTTPRequestHandler
-from urllib.parse import parse_qs, urlparse
+from urllib.parse import parse_qs, quote, urlparse
 
 
 class handler(BaseHTTPRequestHandler):
@@ -16,18 +17,26 @@ class handler(BaseHTTPRequestHandler):
     response = requests.get(api_url)
     data = response.json()
 
-    # Extract the wayback_url and wayback_timestamp
+    # Extract the wayback_url
     wayback_url = ''
-    wayback_timestamp = ''
     if 'archived_snapshots' in data and 'closest' in data['archived_snapshots']:
       wayback_url = data['archived_snapshots']['closest']['url']
-      wayback_timestamp = data['archived_snapshots']['closest']['timestamp']
+
+    # Call the get-image-url endpoint with the wayback_url
+    if wayback_url:
+      image_api_url = f'https://webrewind.vercel.app/api/get-image-url?url={quote(wayback_url)}'
+      image_response = requests.get(image_api_url)
+      image_data = image_response.json()
+
+      # Extract the image_url
+      image_url = image_data.get("image_url", "")
+    else:
+      image_url = ""
 
     # Send the response
     self.send_response(200)
     self.send_header('Content-type', 'application/json')
     self.end_headers()
-    response_json = json.dumps(
-        {'wayback_timestamp': wayback_timestamp, 'wayback_url': wayback_url})
+    response_json = json.dumps({"image_url": image_url})
     self.wfile.write(bytes(response_json, "utf8"))
     return
